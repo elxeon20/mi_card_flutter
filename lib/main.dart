0f5b3d98b680df7db270eefca4894b15dc58e82f
elxeon20 <elxeon20@gmail.com>
2023-07-09 17:59:21 +0300

    09072023

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/avatar.png'),
+            ),
+            Text(
+              "Vitalii Surname",
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "DESCRIPTION OF PERSON",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  color: Colors.white54,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+              width: 350,
+              child: Divider(
+                color: Colors.white38,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 20,
+                ),
+                title: Text(
+                  "+380 99 342 55 99",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text(
+                    "email@gmail.com",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+          ],
+        )),
+      ),
+    );
+  }
+}
